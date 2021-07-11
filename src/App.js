a6f71d7d6e0451100d7686e1b0c9e661351320c1
Вікторія <76106945+ViktoriiaKhimich@users.noreply.github.com>
2021-07-11 22:11:00 +0300

    created collapsible component using recursion to generate data for the component so it can open to n-th level with m children in each node.  Made each node open/close by click on it's name.

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import React, { useState, useCallback } from 'react';
+import { data } from './data'
+
+const CollapsibleComponent = ({ data }) => {
+
+  const [showChildren, setShowChildren] = useState(false)
+
+  const handleClick = useCallback(() => {
+    setShowChildren(!showChildren)
+  }, [setShowChildren, showChildren])
+
+  return (
+    <div className='continentsList'>
+      {data.map(({ id, name }) =>
+        <span key={id} onClick={(id) => handleClick(id)} className='listItem' style={{ backgroundColor: showChildren ? '#DCDCDC	' : '#F7F7F7' }}>
+          <p className='title'>{name}</p>
+        </span>
+      )}
+      <div className='children'>
+        {data.map(({ children }) => {
+          return showChildren && children?.map(child => <CollapsibleComponent data={[child]} />)
+        })}
+      </div>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <CollapsibleComponent data={data} />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const data = [{
+    name: "Europe",
+    id: 1,
+    children: [
+        {
+            id: 1,
+            name: "Adrora",
+            children: [
+                {
+                    id: 1,
+                    name: "Catalan",
+                    country: "Andora"
+                }
+            ]
+        },
+        {
+            id: 2,
+            name: "Albania",
+            children: [
+                {
+                    id: 1,
+                    name: "Albanian",
+                    country: "Albania"
+                },
+                {
+                    id: 2,
+                    name: "Italian",
+                    country: "Albania"
+                }
+            ]
+        },
+    ]
+}]
+
+
